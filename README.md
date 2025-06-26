846792f44b14e1045d42160674d3272c468f2621
Marco Visani <92217074+mvisani@users.noreply.github.com>
2025-06-26 08:39:56 +0200

    added analysis of results function

---
+++ README.md
@@ -0,0 +1,13 @@
+```bash
+docker compose up -d
+```
+
+```bash 
+uv run create_lotus_postgresdb.py
+```
+
+We are currently using MetFrag-2.6.6.
+
+```bash
+uv run run_metfrag_lotus_eval.py --n_jobs N_CPUS
+```
--- run_metfrag_lotus_eval.py
+++ run_metfrag_lotus_eval.py
@@ -1,6 +1,6 @@
 import argparse
 import random
-
+import typing as T
 import pandas as pd
 from downloaders import BaseDownloader
 from joblib import Parallel, delayed
@@ -43,6 +43,123 @@ def main():
     results = Parallel(n_jobs=args.n_jobs)(
         delayed(run_metfrag)(spectrum) for spectrum in tqdm(spectra)
     )
+
+    # we now check the top 1, 5, 10 and 20 results
+    # we also want to check if there is a difference between H adduct or Na adduct
+    # we also want to check if there is a difference between orbitrap and qtof
+    resulting_dataframes = [i[2] for i in results]
+
+    res = analyze_results(spectra, resulting_dataframes)
+    metrics = res.pop("metrics")
+
+
+def analyze_results(
+    spectra: T.List[Spectrum],
+    results: T.List[pd.DataFrame],
+) -> T.Dict[str, T.Union[int, T.Dict[str, int]]]:
+    metrics = {
+        "top_1": 0,
+        "top_5": 0,
+        "top_10": 0,
+        "top_20": 0,
+        "top_1_h": 0,
+        "top_5_h": 0,
+        "top_10_h": 0,
+        "top_20_h": 0,
+        "top_1_na": 0,
+        "top_5_na": 0,
+        "top_10_na": 0,
+        "top_20_na": 0,
+        "top_1_orbitrap": 0,
+        "top_5_orbitrap": 0,
+        "top_10_orbitrap": 0,
+        "top_20_orbitrap": 0,
+        "top_1_qtof": 0,
+        "top_5_qtof": 0,
+        "top_10_qtof": 0,
+        "top_20_qtof": 0,
+    }
+
+    n_spectrum_orbitrap = 0
+    n_spectrum_qtof = 0
+    n_spectrum_h = 0
+    n_spectrum_na = 0
+    for spectrum, res in tqdm(zip(spectra, results), desc="Analyzing results"):
+        # get the metadata from the spectrum
+        inchikey = spectrum.get("inchikey")
+        instrument_type = spectrum.get("instrument_type")
+        adduct = spectrum.get("adduct")
+
+        # add to the total counts
+        if instrument_type == "Orbitrap":
+            n_spectrum_orbitrap += 1
+        elif instrument_type == "QTOF":
+            n_spectrum_qtof += 1
+        if adduct == "[M+H]+":
+            n_spectrum_h += 1
+        elif adduct == "[M+Na]+":
+            n_spectrum_na += 1
+
+        if res.empty:
+            continue
+
+        df_1 = res.iloc[:1]
+        df_5 = res.iloc[:5]
+        df_10 = res.iloc[:10]
+        df_20 = res.iloc[:20]
+
+        if inchikey in df_1["structure_inchikey_1"].values:
+            metrics["top_1"] += 1
+            if adduct == "[M+H]+":
+                metrics["top_1_h"] += 1
+            elif adduct == "[M+Na]+":
+                metrics["top_1_na"] += 1
+            if instrument_type == "Orbitrap":
+                metrics["top_1_orbitrap"] += 1
+            elif instrument_type == "QTOF":
+                metrics["top_1_qtof"] += 1
+
+        if inchikey in df_5["structure_inchikey_1"].values:
+            metrics["top_5"] += 1
+            if adduct == "[M+H]+":
+                metrics["top_5_h"] += 1
+            elif adduct == "[M+Na]+":
+                metrics["top_5_na"] += 1
+            if instrument_type == "Orbitrap":
+                metrics["top_5_orbitrap"] += 1
+            elif instrument_type == "QTOF":
+                metrics["top_5_qtof"] += 1
+
+        if inchikey in df_10["structure_inchikey_1"].values:
+            metrics["top_10"] += 1
+            if adduct == "[M+H]+":
+                metrics["top_10_h"] += 1
+            elif adduct == "[M+Na]+":
+                metrics["top_10_na"] += 1
+            if instrument_type == "Orbitrap":
+                metrics["top_10_orbitrap"] += 1
+            elif instrument_type == "QTOF":
+                metrics["top_10_qtof"] += 1
+
+        if inchikey in df_20["structure_inchikey_1"].values:
+            metrics["top_20"] += 1
+            if adduct == "[M+H]+":
+                metrics["top_20_h"] += 1
+            elif adduct == "[M+Na]+":
+                metrics["top_20_na"] += 1
+            if instrument_type == "Orbitrap":
+                metrics["top_20_orbitrap"] += 1
+            elif instrument_type == "QTOF":
+                metrics["top_20_qtof"] += 1
+
+    return {
+        "metrics": metrics,
+        "n_total": len(spectra),
+        "n_spectrum_orbitrap": n_spectrum_orbitrap,
+        "n_spectrum_qtof": n_spectrum_qtof,
+        "n_spectrum_h": n_spectrum_h,
+        "n_spectrum_na": n_spectrum_na,
+    }
 
 
 if __name__ == "__main__":
