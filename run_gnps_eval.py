9c4020afbb4fbc78632780ff0d2c176334b39354
Marco Visani <marco.visani@unifr.ch>
2026-02-04 17:25:46 +0100

    started gnps evaluation

---
+++ run_gnps_eval.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from matchms.similarity import FlashSimilarity
+
+from ms2mol_evaluation import GNPSEvaluation
+
+
+def main():
+    eval = GNPSEvaluation(
+        Path("evaluations/gnps_lotus"),
+        precursor_mz_tolerance=10.0,
+        precursor_mz_tolerance_type="ppm",
+    )
+    ms2_similarity = FlashSimilarity(
+        score_type="cosine", matching_mode="hybrid", tolerance=0.01
+    )
+    eval.set_ms2_similarity(ms2_similarity)
+    res = eval.run_eval()
+    eval.get_fraction_results(res)
+    eval.get_top_n_results(res)
+
+
+if __name__ == "__main__":
+    main()
